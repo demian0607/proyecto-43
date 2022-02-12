c1918d302cb59976f5c2474476a8f2f4d92c7e09
demian0607 <87732971+demian0607@users.noreply.github.com>
2022-02-11 19:00:18 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: Siempre cambia, no es constante !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("U O N I F N C T", 700,150)
+    fill("lightblue")
+    text("Pista: Realiza una tarea en particular !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A B D T A A E S", 100,250)
+    fill("lightblue")
+    text("Pista: Guarda toda la informacion !!", 100,270)
+    
+
+}
